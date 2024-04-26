4a93aec1c3d672d4ca292774bae68f0e19007e60
jabborov077 <djabborovbehruz2@gmail.com>
2024-04-26 11:02:31 +0500

    js8

---
+++ scripit.js
@@ -0,0 +1,84 @@
+// 'use strict'
+
+// function docket(x,y,z ){
+//     console.log(x+y+z);
+// }
+
+// docket(1,2,3)
+
+// const number = [1,2,3,4,5]
+
+// function docket(x,y,z){
+//     console.log(x+y+z);
+// }
+
+// docket(...number)
+
+// const arr = [1,2,3,4,5]
+// const newArr = [...arr]
+
+// console.log(newArr);
+
+// const islamic = {
+//     name: "Muslim",
+//     age: 30,
+//     isMarried: true,
+//     x:{
+//         a: 1,
+//         b: 2
+//     }
+// }
+
+// const newIslamic = {...islamic}
+
+// newIslamic.name = "Yusuf"
+
+// console.log(newIslamic)
+// console.log(islamic)
+// 'use strict'
+
+// const firstName = "Muslim";
+
+// console.log(typeof firstName)
+
+// const str = new String(firstName);
+
+// console.log(str)
+// console.log(firstName)
+
+// console.log(typeof str)
+// console.log(typeof firstName)
+
+
+// console.dir(String)
+
+// const samsungTv = {
+//     size: 42,
+//     color: "black",
+//     isSmartTv: true,
+//     isConnectable: function (){
+//         console.log("connectable");
+//     }
+// }
+// //
+// const artelTv ={
+//     isSmartTv: false
+// }
+
+
+// artelTv.__proto__ = samsungTv
+
+// artelTv.isConnectable();
+
+
+// iz
+// .
+// Object.setPrototypeOf(artelTv,samsungTv)
+
+// console.log(samsungTv)
+// console.log(artelTv)
+
+
+// const lg = Object.create(samsungTv);
+
+// console.log(lg)
